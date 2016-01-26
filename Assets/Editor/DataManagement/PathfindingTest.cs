c58985717460a1a0358ff76cc3453bb8327355a3
Chris Smith <cedarstreet@gmail.com>
2016-01-26 15:55:59 -0500

    Pathfinding for NPCs
    * Attempting to create a means by which NPCs can pick up goods from a
    station, find a station that buys those goods for less than what they
    bought them for, and construct a route to that sector.

---
+++ Assets/Editor/DataManagement/PathfindingTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathfindingTest : EditorWindow
+{
+    static PathfindingTest editorWindow;
+
+    NodePathfinding np = new NodePathfinding();
+
+    int StartingSectorID = 1;
+    int EndingSectorID = 25;
+
+    [MenuItem("Data Management/Pathing Test")]
+    public static void Init()
+    {
+        editorWindow = EditorWindow.GetWindow<PathfindingTest>();
+        editorWindow.Show();
+    }
+
+    void OnGUI()
+    {
+        StartingSectorID = EditorGUILayout.IntField("Starting ID", StartingSectorID);
+        EndingSectorID = EditorGUILayout.IntField("Ending ID", EndingSectorID);
+        if (GUILayout.Button("Find Path"))
+        {
+            np.StartingSectorID = StartingSectorID;
+            np.EndingSectorID = EndingSectorID;
+            np.BeginRouting();
+        }
+    }
+
+}
---
+++ Assets/Scripts/AI/NodePathfinding.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// Based on the current sector and the desired destination sector,
+/// determine which jumpgates to use to get from point A to point B
+/// http://www.redblobgames.com/pathfinding/a-star/introduction.html
+/// http://www.raywenderlich.com/4946/introduction-to-a-pathfinding
+/// http://heyes-jones.com/astar.php
+/// 
+/// 
+/// </summary>
+public class NodePathfinding : MonoBehaviour 
+{
+    List<int> Open;
+    List<int> Closed;
+
+    List<SectorDataObject> dbSectors;
+    List<JumpgateDataObject> dbJumpgates;
+
+    public int StartingSectorID;
+    public int EndingSectorID;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    
+
+    public void BeginRouting()
+    {
+        Init();
+        GraphIt();
+    }
+
+    void Init()
+    {
+        Open = new List<int>();
+        Closed = new List<int>();
+
+        dbSectorDataObject SectorDatabase = (dbSectorDataObject)Resources.Load(@"AssetDatabases/dbSectorDataItems");
+        dbSectors = SectorDatabase.database;
+
+        dbJumpgateDataObject JumpgateDatabase = (dbJumpgateDataObject)Resources.Load(@"AssetDatabases/dbJumpgateDataItems");
+        dbJumpgates = JumpgateDatabase.database;
+
+
+        Open.Add(StartingSectorID);
+        Closed.Add(StartingSectorID);       
+    }
+
+    void GraphIt()
+    {
+        
+    }
+
+    /// <summary>
+    /// Based on the sector, find neighboring sectors that aren't in the CLOSED list (already dealt with)
+    /// </summary>
+    /// <param name="SectorID"></param>
+    void FindOpenNeighbors(int SectorID)
+    {
+        List<JumpgateDataObject> openJGDO = (from db in dbJumpgates where db.sectorID.Equals(SectorID) select db).ToList<JumpgateDataObject>();
+        foreach(JumpgateDataObject jgdo in openJGDO)
+        {
+            if (!Closed.Contains(jgdo.destinationSectorID))
+            {
+                Open.Add(jgdo.destinationSectorID);
+            }
+        }
+    }
+
+}
