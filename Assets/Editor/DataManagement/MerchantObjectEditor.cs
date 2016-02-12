fab96cf09434e1c30f747e0f29091588218cbfac
Chris Smith <cedarstreet@gmail.com>
2016-02-12 15:46:31 -0500

    Quick Update to Merchant Editor
    I have no idea what the change is, since the change was made last week.
    But I do remember accidentally screwing up the database by deleting a
    record I shouldn't have, so I will be reconciling that ASAP.

--- Assets/Editor/DataManagement/MerchantObjectEditor.cs
+++ Assets/Editor/DataManagement/MerchantObjectEditor.cs
@@ -19,6 +19,9 @@ public class MerchantObjectEditor : EditorWindow {
     dbShieldDataObject dbShields;
     dbPlatingDataObject dbPlating;
 
+    dbSectorDataObject dbSectors;
+    dbStationDataObject dbStations;
+
     MerchantNPCDataObject mnpcdo;
 
     string[] genderOptions = new string[] { "Male", "Female", "Undetermined" };
@@ -42,10 +45,20 @@ public class MerchantObjectEditor : EditorWindow {
     string[] platingNames;
     int[] platingIDs;
 
+    string[] sectorNames;
+    int[] sectorIDs;
+
+    string[] stationNames;
+    int[] stationIDs;
+
+    bool isDockedInSector = false;
+
     Vector2 scrollPos;
 
-    bool isEdit = true;
+    bool isEdit = false;
     int currentlySelectedMerchantID = 0;
+    string saveButtonText = "CREATE";
+    bool okDelete = false;
 
 
     [MenuItem("Data Management/Merchants NPCs")]
@@ -82,13 +95,13 @@ public class MerchantObjectEditor : EditorWindow {
 
         EditorGUILayout.BeginHorizontal();
 
-        Rect leftSide = EditorGUILayout.BeginVertical(GUILayout.Width(Screen.width * 0.25f)); ;
+        Rect leftSide = EditorGUILayout.BeginVertical(GUILayout.Width(Screen.width * 0.40f)); ;
 
         MerchantList();
 
         EditorGUILayout.EndVertical();
 
-        Rect rightSide = EditorGUILayout.BeginVertical(GUILayout.Width((Screen.width * 0.75f) - 30));
+        Rect rightSide = EditorGUILayout.BeginVertical(GUILayout.Width((Screen.width * 0.60f)));
 
         MerchantEditor();
 
@@ -99,16 +112,17 @@ public class MerchantObjectEditor : EditorWindow {
 
     void MerchantList()
     {
-        scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.Width(Screen.width * 0.25f));
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.Width(Screen.width * 0.40f));
 
         EditorGUILayout.BeginVertical();
 
         foreach (MerchantNPCDataObject mnpc in dbMerchants.database)
         {
-            if (GUILayout.Button(mnpc.MerchantLastName + ", " + mnpc.MerchantFirstName)) ;
+            if (GUILayout.Button(mnpc.MerchantLastName + ", " + mnpc.MerchantFirstName))
             {
                 currentlySelectedMerchantID = mnpc.MerchantID;
-
+                isEdit = true;
+                saveButtonText = "UPDATE";
             }
         }
         EditorGUILayout.EndVertical();
@@ -118,55 +132,103 @@ public class MerchantObjectEditor : EditorWindow {
 
     void MerchantEditor()
     {
-        if (GUILayout.Button("New Merchant NPC"))
-        {
-
-        }
-
-        if (currentlySelectedMerchantID > 0 )
+        if (currentlySelectedMerchantID > 0 && isEdit == true)
         {
             mnpcdo = dbMerchants.GetMerchantByID(currentlySelectedMerchantID);
         }
 
-        EditorGUI.BeginDisabledGroup(isEdit == true || currentlySelectedMerchantID > 0);
 
         //#################################################################################
-        //Demographics
+        //Demographics -- Identifiers and such
         mnpcdo.MerchantFirstName = EditorGUILayout.TextField("First name", mnpcdo.MerchantFirstName, GUILayout.Width(300));
         mnpcdo.MerchantLastName = EditorGUILayout.TextField("Last name", mnpcdo.MerchantLastName, GUILayout.Width(300));
-        if (mnpcdo.MerchantGender != null) { genderIndex = (from go in genderOptions select go.IndexOf(mnpcdo.MerchantGender)).First(); }
-        genderIndex = EditorGUILayout.Popup("Gender", genderIndex, genderOptions, GUILayout.Width(300));
         EditorGUILayout.LabelField("Faction will go here");
 
         EditorGUILayout.Space();
 
-        //Here we'll have the ship builder. Everything is based on the values of the 
-        //  selected hull, so the Hull drop down needs to be active when the main form
-        //  is active, and other drop downs are inactive until a valid hull is selected.
-        mnpcdo.HullID = EditorGUILayout.IntPopup("Ship hull", mnpcdo.HullID, hullNames, hullIDs, GUILayout.Width(300));
+        //#################################################################################
+        //Ship -- Hull must be selected for the components to be made available.
+        //  Components will only display those with proper class for the hull
+        mnpcdo.HullID = EditorGUILayout.IntPopup("Ship hull", mnpcdo.HullID, hullNames, hullIDs, GUILayout.Width(350));
+        if (mnpcdo.HullID > 0 ) { FilterShipComponentsByHullClasses(mnpcdo.HullID); }
 
-        EditorGUI.BeginDisabledGroup(mnpcdo.HullID > 0);
+        EditorGUI.BeginDisabledGroup(mnpcdo.HullID == 0);
+        mnpcdo.EngineID = EditorGUILayout.IntPopup("Engine", mnpcdo.EngineID, engineNames, engineIDs, GUILayout.Width(350));
+        mnpcdo.CargoID = EditorGUILayout.IntPopup("Cargo hold", mnpcdo.CargoID, cargoNames, cargoIDs, GUILayout.Width(350));
+        mnpcdo.ShieldID = EditorGUILayout.IntPopup("Shields", mnpcdo.ShieldID, shieldNames, shieldIDs, GUILayout.Width(350));
+        mnpcdo.PlatingID = EditorGUILayout.IntPopup("Plating", mnpcdo.PlatingID, platingNames, platingIDs, GUILayout.Width(350));
+        EditorGUI.EndDisabledGroup();
+
+        EditorGUILayout.Space();
+
+        //#################################################################################
+        //Starting values
 
         EditorGUI.BeginChangeCheck();
-        mnpcdo.EngineID = EditorGUILayout.IntPopup("Engine", mnpcdo.HullID, hullNames, hullIDs, GUILayout.Width(300));
-        if (EditorGUI.EndChangeCheck())
-        {
-            FilterShipComponentsByHullClasses(mnpcdo.HullID);
+        mnpcdo.CurrentSectorID = EditorGUILayout.IntPopup("Starting sector", mnpcdo.CurrentSectorID, sectorNames, sectorIDs, GUILayout.Width(300));
+        if (mnpcdo.CurrentSectorID > 0 ) { FilterStationsBySectorID(mnpcdo.CurrentSectorID); }
+
+        if (mnpcdo.CurrentStationID > 0) { isDockedInSector = true; }
+        isDockedInSector = EditorGUILayout.Toggle("Docked?", isDockedInSector);
+        EditorGUI.BeginDisabledGroup(!isDockedInSector);
+        mnpcdo.CurrentStationID = EditorGUILayout.IntPopup("Docked at", mnpcdo.CurrentStationID, stationNames, stationIDs, GUILayout.Width(300));
+        if (!isDockedInSector) { mnpcdo.CurrentStationID = 0; }
+        EditorGUI.EndDisabledGroup();
+
+        EditorGUILayout.Space();
+
+        mnpcdo.MerchantTickModifier = EditorGUILayout.FloatField("Sim tick modifier", mnpcdo.MerchantTickModifier, GUILayout.Width(200));
+
+        //#################################################################################
+        //Buttons -- Save will change to either CREATE or UPDATE depending on how we started
+        //  CREATE just needs an empty form. UPDATE requires a sidebar selection.
+        //  CLEAR will empty all memorized values
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button(saveButtonText, GUILayout.Width(125))) {
+            GUIUtility.keyboardControl = 0;
+            if (isEdit == true)
+            {
+                UpdateRecord();
+            }
+            else
+            {
+                SaveRecord();
+            }
+
+            ResetForm();
         }
-        //Arrays are null. Fill them with something to start out with
-        mnpcdo.CargoID = EditorGUILayout.IntPopup("Cargo hold", mnpcdo.CargoID, cargoNames, cargoIDs, GUILayout.Width(300));
-        mnpcdo.ShieldID = EditorGUILayout.IntPopup("Shields", mnpcdo.ShieldID, shieldNames, shieldIDs, GUILayout.Width(300));
-        mnpcdo.PlatingID = EditorGUILayout.IntPopup("Plating", mnpcdo.PlatingID, platingNames, platingIDs, GUILayout.Width(300));
+        if (GUILayout.Button("Cancel", GUILayout.Width(125))) { ResetForm(); }
 
-        EditorGUI.EndDisabledGroup();
+        EditorGUILayout.EndHorizontal();
 
+        //###############################################################################################
+        // Delete this station and child shops
 
-        EditorGUI.EndDisabledGroup();
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal();
+        okDelete = EditorGUILayout.BeginToggleGroup("Delete the merchant?", okDelete);
+
+        //Sanity check(mark)
+        if (GUILayout.Button("Delete", GUILayout.Width(125)) && currentlySelectedMerchantID > 0)
+        {
+            DeleteRecord();
+
+            okDelete = false;
+        }
+
+        EditorGUILayout.EndToggleGroup();
+
+        EditorGUILayout.EndHorizontal();
     }
 
     #endregion
 
-    #region Loading
+    #region Database
 
     void LoadResources()
     {
@@ -178,6 +240,9 @@ public class MerchantObjectEditor : EditorWindow {
         dbCargo = (dbCargoModuleDataObject)AssetDatabase.LoadAssetAtPath(DATABASE_PATH + "dbCargoModuleDataItems.asset", typeof(dbCargoModuleDataObject));
         dbShields = (dbShieldDataObject)AssetDatabase.LoadAssetAtPath(DATABASE_PATH + "dbShieldDataItems.asset", typeof(dbShieldDataObject));
         dbPlating = (dbPlatingDataObject)AssetDatabase.LoadAssetAtPath(DATABASE_PATH + "dbPlatingDataItems.asset", typeof(dbPlatingDataObject));
+
+        dbSectors = (dbSectorDataObject)AssetDatabase.LoadAssetAtPath(DATABASE_PATH + "dbSectorDataItems.asset", typeof(dbSectorDataObject));
+        dbStations = (dbStationDataObject)AssetDatabase.LoadAssetAtPath(DATABASE_PATH + "dbStationDataItems.asset", typeof(dbStationDataObject));
     }
 
     void Initialize()
@@ -186,6 +251,24 @@ public class MerchantObjectEditor : EditorWindow {
 
         hullNames = dbHulls.database.Select(x => x.name).ToArray();
         hullIDs = dbHulls.database.Select(x => x.iD).ToArray();
+
+        engineNames = new string[] { "None" };
+        engineIDs = new int[] { 0 };
+
+        cargoNames = new string[] { "None" };
+        cargoIDs = new int[] { 0 };
+
+        shieldNames = new string[] { "None" };
+        shieldIDs = new int[] { 0 };
+
+        platingNames = new string[] { "None" };
+        platingIDs = new int[] { 0 };
+
+        sectorNames = dbSectors.database.Select(x => x.sectorName).ToArray();
+        sectorIDs = dbSectors.database.Select(x => x.sectorID).ToArray();
+
+        stationNames = new string[] { "None" };
+        stationIDs = new int[] { 0 };
     }
 
     void CreateDatabase()
@@ -196,36 +279,117 @@ public class MerchantObjectEditor : EditorWindow {
         AssetDatabase.Refresh();
     }
 
+    void CreateRecord()
+    {
+        currentlySelectedMerchantID = dbMerchants.database.Count > 0 ? dbMerchants.database.Max(x => x.MerchantID) + 1 : 1;
+        mnpcdo = new MerchantNPCDataObject();
+        mnpcdo.MerchantID = currentlySelectedMerchantID;
+        mnpcdo.MerchantFirstName = "New";
+        mnpcdo.MerchantLastName = "Merchant";
+        mnpcdo.MerchantGender = "Undetermined";
+        mnpcdo.MerchantFaction = 0;
+
+        mnpcdo.HullID = 0;
+        mnpcdo.EngineID = 0;
+        mnpcdo.CargoID = 0;
+        mnpcdo.ShieldID = 0;
+        mnpcdo.PlatingID = 0;
+
+        mnpcdo.IsDestroyed = false;
+    }
+
+    void UpdateRecord()
+    {
+        EditorUtility.SetDirty(dbMerchants);
+    }
+
+    void SaveRecord()
+    {
+        mnpcdo.MerchantID = dbMerchants.database.Count > 0 ? dbMerchants.database.Max(x => x.MerchantID) + 1 : 1;
+        dbMerchants.Add(mnpcdo);
+        UpdateRecord();
+    }
+
+    void DeleteRecord()
+    {
+        int idxRecord;
+
+        //Find the object in the database
+        MerchantNPCDataObject tempmnpcdo = dbMerchants.database.Find(x => x.MerchantID.Equals(currentlySelectedMerchantID));
+
+        //Get the index
+        idxRecord = dbMerchants.database.IndexOf(tempmnpcdo);
+        
+        //Remove by index
+        dbMerchants.database.RemoveAt(idxRecord);
+
+        //Update the SOs
+        EditorUtility.SetDirty(dbStations);
+
+        ResetForm();
+    }
+
     void FilterShipComponentsByHullClasses(int HullID)
     {
+        //http://www.devcurry.com/2009/05/replicating-in-operator-in-linq.html
+        //We need to allow for items that are the same class or less than the 
+        //  hull specifies for the class of that slot.
+        //Quality ranges from WORST (1) to BEST (5)
+
+
+
         //Use the HullID to get the Hull record, and use the
         //  hull record to get the classes we can use with that hull.
         HullDataObject hdo = dbHulls.database.Find(x => x.iD.Equals(HullID));
 
-        List<EngineDataObject> okEngines = (from db in dbEngines.database where db.hardwareClass.Equals(hdo.engineClass) select db).ToList<EngineDataObject>();
-        List<CargoDataObject> okCargo = (from db in dbCargo.database where db.hardwareClass.Equals(hdo.cargoClass) select db).ToList<CargoDataObject>();
-        List<ShieldDataObject> okShields = (from db in dbShields.database where db.hardwareClass.Equals(hdo.shieldClass) select db).ToList<ShieldDataObject>();
-        List<PlatingDataObject> okPlating = (from db in dbPlating.database where db.hardwareClass.Equals(hdo.platingClass) select db).ToList<PlatingDataObject>();      
+        List<EngineDataObject> okEngines = (from db in dbEngines.database where db.hardwareClass <= hdo.engineClass select db).OrderByDescending(x=>x.hardwareClass).ToList<EngineDataObject>();
+        List<CargoDataObject> okCargo = (from db in dbCargo.database where db.hardwareClass <= hdo.cargoClass select db).OrderByDescending(x => x.hardwareClass).ToList<CargoDataObject>();
+        List<ShieldDataObject> okShields = (from db in dbShields.database where db.hardwareClass <= hdo.shieldClass select db).OrderByDescending(x => x.hardwareClass).ToList<ShieldDataObject>();
+        List<PlatingDataObject> okPlating = (from db in dbPlating.database where db.hardwareClass <= hdo.platingClass select db).OrderByDescending(x => x.hardwareClass).ToList<PlatingDataObject>();      
 
-        engineNames = okEngines.Select(x => x.name).ToArray();
+        engineNames = okEngines.Select(x => "["+ x.hardwareClass + "] " + x.name + " (T:"+ x.forwardThrust.ToString() + ")").ToArray();
         engineIDs = okEngines.Select(x => x.iD).ToArray();
         engineNames.Union(emptyName);
         engineIDs.Union(emptyID);
 
-        cargoNames = okCargo.Select(x => x.name).ToArray();
+        cargoNames = okCargo.Select(x => "[" + x.hardwareClass + "] " + x.name + " (C:" + x.capacity + ")").ToArray();
         cargoIDs = okCargo.Select(x => x.iD).ToArray();
         cargoNames.Union(emptyName);
         cargoIDs.Union(emptyID);
 
-        shieldNames = okShields.Select(x => x.name).ToArray();
+        shieldNames = okShields.Select(x => "[" + x.hardwareClass + "] " + x.name + " (S:" + x.Strength + ")").ToArray();
         shieldIDs = okShields.Select(x => x.iD).ToArray();
         shieldNames.Union(emptyName);
         shieldIDs.Union(emptyID);
 
-        platingNames = okPlating.Select(x => x.name).ToArray();
+        platingNames = okPlating.Select(x => "[" + x.hardwareClass + "] " + x.name + " (S:" + x.Strength + ")").ToArray();
         platingIDs = okPlating.Select(x => x.iD).ToArray();
         platingNames.Union(emptyName);
         platingIDs.Union(emptyID);
+    }
+
+    void FilterStationsBySectorID(int SectorID)
+    {
+        List<StationDataObject> okStations = (from db in dbStations.database where db.sectorID.Equals(SectorID) select db).ToList<StationDataObject>();
+        if (okStations.Count > 0) { 
+            stationNames = okStations.Select(x => x.stationName).ToArray();
+            stationIDs = okStations.Select(x => x.stationID).ToArray();
+        }
+        stationNames.Union(emptyName);
+        stationIDs.Union(emptyID);
+    }
+
+    #endregion
+
+    #region Utility
+
+    void ResetForm()
+    {
+        isEdit = false;
+        currentlySelectedMerchantID = 0;
+        saveButtonText = "CREATE";
+        isDockedInSector = false;
+        mnpcdo = new MerchantNPCDataObject();
     }
 
     #endregion
